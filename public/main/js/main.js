9acc8e7a139d6d1e078cd1654d3352ad01faabef
Karan Jeet <karanjeet96@gmail.com>
2015-11-04 01:03:41 +0530

    Adding express session module

--- public/main/js/main.js
+++ public/main/js/main.js
@@ -20,47 +20,7 @@
 
            /*-------------- form submit functions --------------*/
             
-            $(function() {
-
-
-            $('#form_registerArtist').submit(function () {
-                var data = JSON.stringify($(this).serializeObject());
-                //console.log(data);
-                $.ajax({
-                    type: 'POST',
-                    url: '/addartist2',
-                    dataType: 'json',    //not req
-                    data: data
-                })
-                .done(function (res) {
-                    alert('Success!! ' + res);
-                    console.log(data);
-                })
-                return false;
-            });
-
-            /*--------------- form data to JSON ---------------*/
-
-             $.fn.serializeObject = function()
-             {
-                var o = {};
-                var a = this.serializeArray();
-                $.each(a, function() {
-                    if (o[this.name] !== undefined) {
-                        if (!o[this.name].push) {
-                            o[this.name] = [o[this.name]];
-                        }
-                        o[this.name].push(this.value || '');
-                    } else {
-                        o[this.name] = this.value || '';
-                    }
-                });
-                return o;
-             };
-
-            /*------------- / . form data to JSON ------------*/
-            });
-
+           
            /*------------------/ . ---------------------------*/
 
            
